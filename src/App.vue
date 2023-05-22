3cd9625ee301cbdb734a980d36d5e5a17c245fa0
Rose Vergel <50724307+lialeon84@users.noreply.github.com>
2023-05-22 17:34:52 -0500

    pushing vue app

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+   
+    <nav class="flex justify-between items-center py-8">
+    <!-- <div class="container px-4 mx-auto flex flex-wrap items-center justify-between"> -->
+      <div class="font-bold text-lg text-indigo-500">
+        <!-- <RouterLink to="/">Home</RouterLink> -->
+      
+      </div>
+      <div class="flex items-center gap-6 text-indigo-500">
+       
+        <!-- <RouterLink to="/about">About</RouterLink> -->
+      </div>
+    <!-- </div> -->
+  </nav>
+
+  
+
+
+  </header>
+
+  <RouterView />
+</template>
+
+
+<script>
+
+</script>
+
+
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="flex justify-center items-center h-90">
+  <div class="max-w-sm p-6 h-80 w-80 rounded-lg shadow bg-gray-800">
+        <img class="w-24 h-24 rounded-full mx-auto" v-bind:src="picture"/>
+       
+        <h1 class="mb-2 text-2xl font-bold text-center tracking-tight text-gray-900 dark:text-white">{{ firstName }} {{ lastName }}</h1>
+      
+         
+          <h3 class="font-normal text-center text-gray-700 dark:text-gray-400">{{ phone }}</h3>
+          <h3 class="font-normal text-center text-gray-700 dark:text-gray-400">{{ email }}</h3>
+      
+   <br>
+   <div class="flex justify-center items-center">
+    <button class="rounded-full bg-gradient-to-br from-[#00DBDE] to-[#FC00FF] px-5 py-3 text-base font-medium text-white transition duration-200 hover:shadow-lg hover:shadow-[#FC00FF]/50  mb-80" v-on:click="getUser()">Get Random User</button>
+  </div>
+
+  </div>
+  
+</div>
+  
+</template>
+
+<script>
+export default {
+  
+  data() {
+    return {
+      firstName: "Rose",
+      lastName: "Leon",
+      email: "test@example.com",
+      phone: "222-222-2222",
+      picture: "https://randomuser.me/api/portraits/med/women/1.jpg",
+    }
+  },
+  methods: {
+    async getUser() {
+      
+      let res = await fetch('https://randomuser.me/api/')
+        let {results} = await res.json()
+        // console.log(results)
+      
+        this.firstName = results[0].name.first
+        this.lastName = results[0].name.last
+        this.email = results[0].email
+        this.phone = results[0].phone
+        this.picture = results[0].picture.medium
+    }
+  }
+}
+</script>
